31586d6f528d7ff4e4697f4605df39f67f29dbca
ProgramerPlus <hipitelo@outlook.com>
2025-08-22 22:35:04 -0300

    Hello World final

--- Program.cs
+++ Program.cs
@@ -1,11 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Mongaguá");
-Console.WriteLine("Itanhaém");
-Console.WriteLine("Peruíbe");
-Console.WriteLine("Praia Grande");
-Console.WriteLine("Santos");
-Console.WriteLine("São Vicente");
-Console.WriteLine("Guarujá");
-Console.WriteLine("Bertioga");
-Console.WriteLine("Iguape");
-Console.WriteLine("Cananéia");
+Console.WriteLine("Olá você");
+
