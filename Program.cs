1dddf8b580d2940b8e4d5f7873652a1e33938445
ProgramerPlus <hipitelo@outlook.com>
2025-08-22 22:29:37 -0300

    Cidades do Litoral Sul

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Olá você");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Iguape");
+Console.WriteLine("Cananéia");
